25f7fd22937840fee5f3256abe8533e68f946e88
Bui Bao Khang <bbk51204@gmail.com>
2026-04-29 13:07:53 +0700

    Sửa giao diện ngừoi dùng, sửa phần tin tức, mã giảm giá

--- lib/Views/customer_dashboard_view.dart
+++ lib/Views/customer_dashboard_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:intl/intl.dart';
 import '../Controllers/auth_controller.dart';
 import '../Controllers/product_controller.dart';
